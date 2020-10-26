c9c29aab0de55c44e2a977b7bd621e28b5e78f3a
kana.nakajima <taro.kirameki@techacademy.jp>
2020-10-26 14:19:26 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_100653) do
+ActiveRecord::Schema.define(version: 2020_10_26_133823) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
