94a6e83a1ff78bf986293faf3e784f7e40977fdc
kana.nakajima <taro.kirameki@techacademy.jp>
2020-11-09 14:40:58 +0000

    login function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,31 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.all
+      @task = current_user.tasks.build  # form_with用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
-
+  
   def show
+     @task = Task.find(params[:id])
+  end
+  
+  def edit
+     @task = Task.find(params[:id])
+  end
+  
+  def update
+     @task = Task.find(params[:id])
+     @task.attributes = task_params
+     if @task.save
+      flash[:success] = 'タスクを正常に更新しました。'
+      redirect_to root_url
+    else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの更新に失敗しました。'
+      render 'tasks/index'
+    end
   end
 
   def new
@@ -13,44 +33,36 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスクを作成しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの作成に失敗しました。'
+      render action: :index
     end
   end
 
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'メッセージを削除しました。'
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    redirect_to tasks_path
   end
 
   private
 
-  def set_task
-    @task = Task.find(params[:id])
+  def task_params
+    
+    params.require(:task).permit(:content)
   end
 
-  def task_params
-    params.require(:task).permit(:status, :content)
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
